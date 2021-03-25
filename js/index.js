5369330db05df2091898c811e60ae874d6a27b30
hamzahasa <hamzahasa067@gmail.com>
2021-03-25 06:03:25 +0200

    added pricing section

--- js/index.js
+++ js/index.js
@@ -6,4 +6,20 @@ toggler.addEventListener("click" , e => {
 } )
 
 
+//animations
+const Showitems = document.querySelectorAll(".hide")
+
+
+
+//faqs
+const faq_items = document.querySelectorAll(".faq-item")
+faq_items.forEach(item => {
+    item.addEventListener("click" , e => {
+        if(e.target.classList.contains("btn")) {
+            e.target.classList.toggle("active")
+           item.querySelector(".answer").classList.toggle("active")
+        }
+    })
+})
+
 
